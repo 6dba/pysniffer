0a39562cb41ed66a824c8d3ddd6b2c0a4fd32184
6dba <pasha.panukov@gmail.com>
2023-03-21 15:45:03 +0300

    Restore the repository, GUI with color

--- sniffer.py
+++ sniffer.py
@@ -3,12 +3,15 @@ from scapy.all import *
 from scapy.layers.http import HTTPRequest
 from datetime import datetime
 from scapy.layers.inet import IP
-
+from colorama import Fore
 import PySimpleGUI as sg
 
 global is_raw
 
-
+GREEN = Fore.GREEN
+RED = Fore.RED
+RESET = Fore.RESET
+WHITE = Fore.WHITE
 def sniff_packets(interface: Optional[str] = None) -> None:
     """
     Перехват пакетов 80 портов с помощью 'iface', если None (по умолчанию), то
@@ -18,7 +21,7 @@ def sniff_packets(interface: Optional[str] = None) -> None:
     :rtype: None
     """
     window['output'].print(
-        f"\n[!] Start sniffing HTTP with interface {interface if interface else 'scapy'}")
+        f"\n[!] Start sniffing HTTP with interface {interface if interface else 'scapy'}", text_color='red')
     window.refresh()
 
     sniff(filter="port 80", prn=process_packet, iface=interface, store=False)
@@ -38,8 +41,9 @@ def process_packet(packet: Any) -> None:
         dst = packet[IP].dst
         method = packet[HTTPRequest].Method.decode()
 
-        window['output'].print(
-            f"\n[+] [{datetime.now()}] src: {src} Requested {url} dst: {dst} with {method}")
+        window['output'].print(f"\n[+] [{datetime.now()}] ", text_color='red')
+        window['output'].print(f"src: {src} Requested {url} dst: {dst}", text_color='blue')
+        window['output'].print(f"with {method}", text_color='green')
         window.refresh()
 
         if is_raw and packet.haslayer(Raw) and method == "POST":
